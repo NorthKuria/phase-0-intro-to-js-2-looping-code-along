0b4b611e6b029bd598c071f981cec250504bf9b7
northkuria <kuri4martin@gmail.com>
2022-06-05 09:18:25 +0300

    added for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ['Charlie', 'Samip', 'Ali']
+
+function writeCards(names, event) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return messages
+}
+
+writeCards(names)
+
+function countDown(x) {
+  while (x >= 0) {
+    console.log(x)
+    x--
+  }
+}
